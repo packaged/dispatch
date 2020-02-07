edc5052181073fd4f57ffc7d3940913e7e7231f0
Brooke Bryan <brooke@bajb.net>
2020-02-07 13:06:51 +0000

    Test for the bug

---
+++ tests/Resources/NestedResourceTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Packaged\Dispatch\Tests\Resources;
+
+use Packaged\Dispatch\Dispatch;
+use Packaged\Dispatch\ResourceManager;
+use Packaged\Dispatch\Resources\CssResource;
+use Packaged\Helpers\Path;
+use PHPUnit\Framework\TestCase;
+
+class NestedResourceTest extends TestCase
+{
+  public function testProcessesContent()
+  {
+    $root = Path::system(dirname(__DIR__), '_root');
+    Dispatch::bind(new Dispatch($root, '/_r/'))->addAlias('root', Path::system($root, Dispatch::RESOURCES_DIR));
+    $manager = ResourceManager::resources();
+
+    $resource = new CssResource();
+    $resource->setManager($manager);
+    $resource->setProcessingPath('css/test.css');
+    $resource->setContent(file_get_contents(Path::system($root, Dispatch::RESOURCES_DIR, 'css', 'test.css')));
+    $content = $resource->getContent();
+    $this->assertContains('url(/_r/r/395d1a0e8999/img/x.jpg?x=y&request=b)', $content);
+  }
+}
